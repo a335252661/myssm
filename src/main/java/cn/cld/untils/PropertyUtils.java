9aba5cff757a797fc0cdebe260c98dfa6ed3101a
chengliude <335252661@qq.com>
2019-03-07 15:39:03 +0800

    描述信息

---
+++ src/main/java/cn/cld/untils/PropertyUtils.java
@@ -0,0 +1,60 @@
+package cn.cld.untils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.Properties;
+
+
+public  class mmm {
+
+    private static final Logger logger = LoggerFactory.getLogger(mmm.class);
+    private static Properties props;
+    static{
+        loadProps();
+    }
+
+    synchronized static private void loadProps(){
+        props = new Properties();
+        InputStream in = null;
+        try {
+//　　　　　　　<!--第一种，通过类加载器进行获取properties文件流-->
+                    in = mmm.class.getClassLoader().getResourceAsStream("system.properties");
+//　　　　　　  <!--第二种，通过类进行获取properties文件流-->
+                    //in = PropertyUtil.class.getResourceAsStream("/jdbc.properties");
+                    props.load(in);
+        } catch (FileNotFoundException e) {
+            logger.error("jdbc.properties文件未找到");
+        } catch (IOException e) {
+            logger.error("出现IOException");
+        } finally {
+            try {
+                if(null != in) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                logger.error("jdbc.properties文件流关闭出现异常");
+            }
+        }
+    }
+
+    public static String getProperty(String key){
+        if(null == props) {
+            loadProps();
+        }
+        return props.getProperty(key);
+    }
+
+    public static String getProperty(String key, String defaultValue) {
+        if(null == props) {
+            loadProps();
+        }
+        return props.getProperty(key, defaultValue);
+    }
+}
